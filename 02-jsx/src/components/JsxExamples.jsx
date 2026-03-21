305e69df41a7e65faa5bbe502fcf033f04cd6fe7
Jefferson Frade <jefferrson.frade@gmail.com>
2026-03-21 00:50:48 -0300

    JSX

--- 02-jsx/src/components/JsxExamples.jsx
+++ 02-jsx/src/components/JsxExamples.jsx
@@ -14,14 +14,58 @@ const JsxExamples = () => {
         return `Olá ${name}`;
     }
 
+    const userIsLoggedIn = true;
+    const userRole = 'admin';
+    const users = [
+        {id: 1, name: 'Fulano'},
+        {id: 2, name: 'Ciclano'},
+        {id: 3, name: 'Beltrano'},
+    ]
+
     return (
         <div>
             <p>O nome do usuário é: {userName}</p>
             <p>Usuário: {user.name} {user.lastName}</p>
 
-            {/** Tudo dentro da interpolação {} é JS */}
+            {/** Tudo dentro da interpolação {} é código JS */}
             <p>{2 + 2}</p>
             <p>{getGreeting(userName)}</p>
+
+            {/* Diferenças do HTML */}
+            {/* Como JSX roda JS por baixo dos panos, há instruções que não podem ser passadas. Ex: class */}
+
+            {/* Exemplo estilizando uma div */}
+            <div className="alguma-coisa">Div</div>
+
+            {/* Todo e qualquer atributo, sempre em camel case (nomeDoAtributo) */}
+            <button onClick={() => alert('Teste')}>Clique</button>
+
+            {/* Toda e qualquer tag deve ser encerrada. Ex: <input/> */}
+            <input type="text" placeholder="Digite Algo"/>
+
+            {/* Renderização condicional */}
+            {/* É uma boa prática renderizar JSX dentro de () */}
+            {userIsLoggedIn ? (
+                <p>Logado</p>
+            ): (
+                <p>Deslogado</p>
+            )}
+
+            {/* Fazendo condicional com if sem else */}
+            <p>{userRole == 'admin' && 'Você é um admin'}</p>
+
+            {/* Renderização de listas */}
+            <div>
+                <ul>
+                    {/* Usando destructuring */}
+                    {users.map((user) => (
+                        // Sem o key, é gerado um warning, é necessário um identificador único em cada elemento. (É uma boa prática)
+                        <li key={user.id}>
+                            {user.id} - {user.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
